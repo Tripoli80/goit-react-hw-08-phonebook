d22c2c4669bf1c5f2eaf376d4762c8f3768b3c37
Tripoli80 <tripoli80@gmail.com>
2022-09-30 14:28:21 +0200

    +++

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,56 +1,61 @@
-import React, { Component } from "react";
-import { InputName, ContactFormAdd, SubmitBtn } from "./ContactForm.styled";
-export default class ContactForm extends Component {
-  state = {
-    name: "",
-    number: "",
+import { useState } from 'react';
+import { InputName, ContactFormAdd, SubmitBtn } from './ContactForm.styled';
+
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const onChangeForma = obj => {
+    const { value } = obj.target;
+
+    switch (obj.target.name) {
+      case 'name': {
+        setName(value);
+        break;
+      }
+      case 'number': {
+        setNumber(value);
+        break;
+      }
+      default:
+        console.log(`Якась помилка з назвою інпута`)
+    }
   };
 
-  onChangeForma = (obj) => {
-    const { name, value } = obj.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-  onResetInput = () => {
-    this.setState({
-      name: "",
-      number: "",
-    });
+  const onResetInput = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    const { onAddContact } = this.props;
-    const { onChangeForma, onResetInput } = this;
-    return (
-      <div>
-        <ContactFormAdd
-          onSubmit={(e) => onAddContact({ e: e, onResetInput: onResetInput })}
-        >
-          <InputName
-            placeholder="Name"
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={(e) => onChangeForma(e)}
-          />
-          <InputName
-            placeholder="Phone Number"
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            value={number}
-            required
-            onChange={(e) => onChangeForma(e)}
-          />
-          <SubmitBtn> Add to my contact</SubmitBtn>
-        </ContactFormAdd>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ContactFormAdd
+        onSubmit={e => onAddContact({ e: e, onResetInput: onResetInput })}
+      >
+        <InputName
+          placeholder="Name"
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={e => onChangeForma(e)}
+        />
+        <InputName
+          placeholder="Phone Number"
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={number}
+          required
+          onChange={e => onChangeForma(e)}
+        />
+        <SubmitBtn> Add to my contact</SubmitBtn>
+      </ContactFormAdd>
+    </div>
+  );
+};
+
+export default ContactForm;
